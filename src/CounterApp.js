7838794672f026cf0d5e296074ec9a8c597e2f21
Turiano Romero <romero.guzman18@gmail.com>
2020-09-11 16:52:40 -0500

    finish course

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -8,11 +8,21 @@ const CounterApp = ({value}) => {
     setIncrement(increment + 1)
   }
 
+  const handleSubtract = () => {
+    setIncrement(increment - 1);
+  };
+  
+  const handleReset = () => {
+    setIncrement(value);
+  }
+
   return (
     <>
       <h1>Counter App</h1>
       <h2>{increment}</h2>
       <button onClick={handleAdd}>+ 1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleSubtract}>- 1</button>
     </>
   );
 }
