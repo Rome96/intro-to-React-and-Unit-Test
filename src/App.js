d2ed04b8df317f0fcfba55d787e706473146218c
Turiano Romero <romero.guzman18@gmail.com>
2020-09-10 23:20:16 -0500

    print of variables in html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const App = () => {
+  const greeting = 'Hola universo'
   return (
     <>
-      <h1> Hola turi</h1>
+      <h1>{greeting}</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         eligendi nisi reiciendis sunt illum delectus aspernatur et vel suscipit.
