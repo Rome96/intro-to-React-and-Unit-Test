b1aaebd6f529cc1add5904914836670826ecfa1d
Turiano Romero <romero.guzman18@gmail.com>
2020-09-10 23:51:05 -0500

    default props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const App = ({greeting}) => {
+const App = ({greeting, subTitle}) => {
   // const greeting = 'Hola universo'
   return (
     <>
       <h1>{greeting}</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        eligendi nisi reiciendis sunt illum delectus aspernatur et vel suscipit.
-      </p>
+      <p>{subTitle}</p>
     </>
   )
 }
 
 App.propTypes = {
   greeting: PropTypes.string.isRequired
+}
+
+App.defaultProps = {
+  subTitle: 'Lorem ipsum dolor repudiandae debitis molestiae illo nobis dolorum esse Voluptate'
 }
 
 export default App
