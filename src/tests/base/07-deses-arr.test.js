7db5b0cebceb6217c07f3ba92b6404cec67edfe8
Turiano Romero <romero.guzman18@gmail.com>
2020-09-16 23:17:19 -0500

    test types data array

--- src/tests/base/07-deses-arr.test.js
+++ src/tests/base/07-deses-arr.test.js
@@ -2,7 +2,7 @@ import { returnArray } from '../../base/07-deses-arr';
 
 describe('Testing archive 07-deses-arr', () => {
   test("the fn returnArray return a number and string ", () => {
-    
+     
     const [letters, numbers] = returnArray(); // ["ABC", 123]
 
     expect(letters).toBe("ABC");
