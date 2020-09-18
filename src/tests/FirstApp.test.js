5f402c85781a303e6954684ddbab90717d0f2f91
Turiano Romero <romero.guzman18@gmail.com>
2020-09-18 00:19:24 -0500

    install and configure enzyme, using Snapshot

--- src/tests/FirstApp.test.js
+++ src/tests/FirstApp.test.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import '@testing-library/jest-dom'
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 import FirstApp from '../FirstApp';
-import { render } from '@testing-library/react';
 
-describe('Testing in component FirstApp', () => {
+describe('Testing in component <FirstApp/>', () => {
+
+  // test('FirstApp return Hola, soy Turiano', () => {
+  //   const greeting = 'Hola, soy Turiano';
+  //   const { getByText } = render(<FirstApp greeting={greeting} />);
+
+  //   expect(getByText(greeting)).toBeInTheDocument()
+  // });
+
   test('FirstApp return Hola, soy Turiano', () => {
-    const greeting = 'Hola, soy Turiano';
-    const { getByText } = render(<FirstApp greeting={greeting} />);
-
-    expect(getByText(greeting)).toBeInTheDocument()
+    const greeting = "Hola, soy Turiano";
+    const wrapper = shallow(<FirstApp greeting ={greeting}/>);
+    expect(wrapper).toMatchSnapshot()
   });
+  
+
 });
